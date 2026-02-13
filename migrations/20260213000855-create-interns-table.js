2bfe033c1d5fbb9cadeaf213488f7322feb012d7
mdivyaswarupa2004 <divyaswarupa@everest.engineering>
2026-02-13 06:44:44 +0530

    Add migration to create interns table

---
+++ migrations/20260213000855-create-interns-table.js
@@ -0,0 +1,42 @@
+"use strict";
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("interns", {
+      id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+        allowNull: false
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      nickname: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true
+      },
+      dob: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false
+      }
+    });
+  },
+  async down(queryInterface) {
+    await queryInterface.dropTable("interns");
+  }
+};
